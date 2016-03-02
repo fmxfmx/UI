e447617090d22e5af8714afbb9df4dcef10b6a20
fmxfmx <fengmingxiao@fengmingxiaodemac-mini.local>
2016-03-02 14:23:10 +0800

    Initial Commit

---
+++ UI布局探究/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI布局探究
+//
+//  Created by fengmingxiao on 16/3/2.
+//  Copyright © 2016年 fengmingxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI布局探究Tests/UI____Tests.m
@@ -0,0 +1,39 @@
+//
+//  UI____Tests.m
+//  UI布局探究Tests
+//
+//  Created by fengmingxiao on 16/3/2.
+//  Copyright © 2016年 fengmingxiao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UI____Tests : XCTestCase
+
+@end
+
+@implementation UI____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
